ed3fbbabfa45a84ff1ea9275dd39e3a36d123b66
thanhhuy44 <90882506+thanhhuy44@users.noreply.github.com>
2022-08-06 14:00:45 +0700

    update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import webRoutes from "./src/routes/webRoutes.js";
 
 const app = new express();
@@ -7,6 +8,7 @@ dotenv.config();
 
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.listen(port, () => {
   console.log("app listen on port " + port);
 });
