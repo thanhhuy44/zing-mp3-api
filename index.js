c892653fd1ebd6b04b137aa3cb66002cb9e4af61
thanhhuy44 <90882506+thanhhuy44@users.noreply.github.com>
2022-08-27 06:56:30 +0700

    update

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import ejs from "ejs";
 import webRoutes from "./src/routes/webRoutes.js";
 
 const app = new express();
 dotenv.config();
 
 const port = process.env.PORT || 3000;
-
+app.set("view engine", "ejs");
 app.use(cors());
+app.get("/", (req, res) => res.render("PlaylistMusic-main/index"));
 app.listen(port, () => {
   console.log("app listen on port " + port);
 });
